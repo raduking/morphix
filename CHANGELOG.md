cdfc82a28c20f30b1c2809f8544e8291f785a914
Radu Sebastian LAZIN <raduking@gmail.com>
2025-04-16 19:19:53 +0300

    Increased parent version to 3.4.4 (#5)
    * Increased parent version to 3.4.4
    
    * Added `Methods.getCallerMethodName` with skipFrames parameter.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,10 @@
 - In development.
 - `Reflection.findSubclass` no longer throws `IllegalStateException` it throws `ReflectionException` instead.
 - Added `Nullables.requireNull` for easier preconditions throwing an `IllegalArgumentException` if the parameter is not `null`.
+- Added `JavaObjects.max` that returns the maximum of two `Comparable`s
+- Added `JavaObjects.min` that returns the minimum of two `Comparable`s
+- Updated parent to `3.4.4`
+- Added `Methods.getCallerMethodName` with skipFrames parameter.
 
 ---
 
--- src/test/java/org/morphix/lang/JavaObjectsMinTest.java
+++ src/test/java/org/morphix/lang/JavaObjectsMinTest.java
@@ -12,39 +12,44 @@
  */
 package org.morphix.lang;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.notNullValue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 /**
- * Test class for {@link JavaObjects}.
+ * Test class for {@link JavaObjects#max(Comparable, Comparable)}.
  *
  * @author Radu Sebastian LAZIN
  */
-class JavaObjectsTest {
+class JavaObjectsMinTest {
 
 	@Test
-	void shouldCastToInferredType() {
-		Object o = "Bubu";
+	void shouldReturnMinimum() {
+		Integer x = 10;
+		Integer y = 20;
 
-		String bubu = JavaObjects.cast(o);
+		Integer result = JavaObjects.min(x, y);
 
-		assertThat(bubu, notNullValue());
-		assertThat(bubu, equalTo("Bubu"));
+		assertThat(result, equalTo(x));
 	}
 
 	@Test
-	void shouldThrowClassCastExceptionWhenObjectCannotBeCastToInferredType() {
-		String x = "Cucu";
+	void shouldReturnMinimumSecondWhenFirstParamIsNull() {
+		Integer y = 20;
 
-		ClassCastException e = assertThrows(ClassCastException.class, () -> {
-			@SuppressWarnings("unused")
-			Integer xi = JavaObjects.cast(x);
-		});
+		Integer result = JavaObjects.min(null, y);
 
-		assertThat(e, notNullValue());
+		assertThat(result, equalTo(y));
 	}
+
+	@Test
+	void shouldReturnMinimumFirstWhenSecondParamIsNull() {
+		Integer x = 10;
+
+		Integer result = JavaObjects.min(x, null);
+
+		assertThat(result, equalTo(x));
+	}
+
 }
