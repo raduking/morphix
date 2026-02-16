59da9e2aff3ac1c81fd0ebc7ef25c3859b88bc1f
raduking <radu.lazin@boatyardx.com>
2026-02-16 17:01:56 +0200

    Updated JavaDoc.

--- src/test/java/org/morphix/convert/handler/IterableToIterableQueueTest.java
+++ src/test/java/org/morphix/convert/handler/IterableToIterableQueueTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 the original author or authors.
+ * Copyright 2026 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
  * the License. You may obtain a copy of the License at
@@ -48,8 +48,9 @@ class IterableToIterableQueueTest {
 		@Override
 		public boolean equals(final Object obj) {
 			// basic equals implementation
-			if (null == obj)
+			if (null == obj) {
 				return false;
+			}
 			return Objects.equals(x, ((B) obj).x);
 		}
 
